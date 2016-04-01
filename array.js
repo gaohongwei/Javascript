b13fcc708c300f11fe8de0687d4351355963ca51
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2016-04-01 15:41:12 -0700

    Create array.js

---
+++ array.js
@@ -0,0 +1,4 @@
+// array.map(callbackfn),    create a new array by calling callbackfn on each element of original array
+// array.filter(callbackfn), create a new array by selecting elements from original array
+// function callbackfn(value, index, array1)
+// array
