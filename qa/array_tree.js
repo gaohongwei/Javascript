d0050f7ee8c086b92518adfbe1fc8c063076f568
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-01-06 14:40:25 -0800

    Update array_tree.js

--- qa/array_tree.js
+++ qa/array_tree.js
@@ -1,27 +1,30 @@
- data = [{id: 1, pid:-1, children:[] },
+ nodes = [{id: 1, pid:-1, children:[] },
        {id: 2, pid:1, children:[] },
        {id: 3, pid:1, children:[] },
        {id: 4, pid:1,  children:[] }];
-Fill children array for each node.   
-Return root node.   
-root is: item.pid == -1;
 
-My solution:
-function build(data) {
+
+Fill children array for each node.   
+Return root nodes.   
+root is: node.pid == -1;
+
+function build_tree(nodes) {
      var index=0;
-     var len = data.length;
-     var hs={};
+     var len = nodes.length;
+     var map={};
+     var roots = [];
      for(index=0;index<len;index++){
-         var item = data[index];
-         if (hs[item.pid] == 'undefined') hs[item.pid] = [];
-         hs[item.pid].push(item.id);
+         var node = nodes[index];
+         if (map[node.pid] == 'undefined') map[node.pid] = [];
+         map[node.pid].push(node.id);
      }
      
      for(index=0;index<len;index++){
-         var item = data[index];
-         item.children = hs[item.id];
+         var node = nodes[index];
+         node.children = map[node.id];
      }
      
-     var root = Math.min(Object.keys(hs));
-     var rootNode = data.find(function(item){item.id==root});     
+     var root = Math.min(Object.keys(map));
+     var rootNode = nodes.find(function(node){node.id==root});     
 }
+
