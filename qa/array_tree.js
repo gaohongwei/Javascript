169161d03a8dd4bffaf7e109ff722d5edea78d89
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-01-06 14:44:24 -0800

    Update array_tree.js

--- qa/array_tree.js
+++ qa/array_tree.js
@@ -9,6 +9,23 @@ Return root nodes.
 root is: node.pid == -1;
 
 function build_tree(nodes) {
+     var map={};
+     var roots = [];
+     var index;
+     for(index=0;index<nodes.length;index++){
+         var node = nodes[index];
+         if (map[node.pid] == undefined) map[node.pid] = [];
+         map[node.pid].push(node.id);
+         if (node.pid < 0) roots.push(node)
+     }
+     
+     for(index=0;index<len;index++){
+         var node = nodes[index];
+         node.children = map[node.id];
+     }   
+}
+
+function build_tree_v1(nodes) {
      var index=0;
      var len = nodes.length;
      var map={};
@@ -27,4 +44,3 @@ function build_tree(nodes) {
      var root = Math.min(Object.keys(map));
      var rootNode = nodes.find(function(node){node.id==root});     
 }
-
