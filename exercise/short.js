c40e81d179efd3618b59bda2bb33007b9dc4cb92
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-01-06 15:13:31 -0800

    Update short.js

--- exercise/short.js
+++ exercise/short.js
@@ -9,3 +9,10 @@ The function accepts an integer that specifies how many times the string has to 
 The function returns the string repeated the number of times specified. For example:
 console.log('hello'.repeatify(3));
 Should print hellohellohello.
+
+String.prototype.repeatify = String.prototype.repeatify || function(num){
+  var str='';
+    for(var index=0;index<num;index++) str +=this;
+    return str;
+}
+var ss = 'hello'.repeatify(3)
