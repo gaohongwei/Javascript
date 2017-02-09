4bae339a2779d4ab15d9a588cc0b22bbdc774bf1
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-02-08 22:05:17 -0800

    Update delegation.js

--- event/delegation.js
+++ event/delegation.js
@@ -1,6 +1,10 @@
+Events bubble
+The majority of browser events propagate from the deepest, 
+innermost element (the event target) to the body and the document element. 
 
 dom.on( events [, selector ] [, data ], handler )
 A selector string to filter the descendants of the selected elements that trigger the event. 
+If selector is omitted or is null, the event handler is referred to as direct or directly-bound. 
 
 $('ul').on('click', 'li' ,start_process)
 
