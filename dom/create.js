890106623522aa7a25b303c7e13a5135028e18fa
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-02-14 19:59:06 -0800

    Update create.js

--- dom/create.js
+++ dom/create.js
@@ -1,1 +1,12 @@
-
+$('#tba').click(f_click);
+function f_click(){
+  var ar=['111','222','333'];  
+  var target=$(this);
+  var dom_ul=$('<ul>');
+  var index,len=ar.length;
+  for(index=0;index<len;index++){
+    var dom_li=$('<ul>', {text: ar[index], id:ar[index] });
+    dom_ul.append(dom_li);
+  }
+  target.append(dom_ul);
+}
