0e5b207c7a0d61edc5e584449315286dbd5f71b5
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-02-14 23:17:35 -0800

    Update create_doms.js

--- dom/create_doms.js
+++ dom/create_doms.js
@@ -1,27 +1,28 @@
 $('#tba').click(function(){
   var target=$(this);
   create_list(target);
-
 });
 
 function create_list(target){
   var ar=['111','222','333'];
-  var dom_parent=$('<ul>');
+  var dom_top=$('<ul>');
   var index,len=ar.length;
   for(index=0;index<len;index++){
     var dom_child=$('<ul>', {text: ar[index], id:ar[index] });
-    dom_parent.append(dom_child);
+    dom_top.append(dom_child);
   }
-  target.append(dom_parent);
+  target.children().remove()
+  target.append(dom_top);
 }
 
 function create_select(target){
   var ar=['111','222','333'];
-  var dom_parent=$('<select>');
+  var dom_top=$('<select>');
   var index,len=ar.length;
   for(index=0;index<len;index++){
     var dom_child=$('<option>', {text: ar[index], id:ar[index] });
-    dom_parent.append(dom_child);
+    dom_top.append(dom_child);
   }
-  target.append(dom_parent);
+  target.children().remove()
+  target.append(dom_top);
 }
