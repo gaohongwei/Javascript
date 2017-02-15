c5f4d436a6f551176450d313001478b636b69501
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-02-14 23:26:29 -0800

    Update sample.js

--- ajax/sample.js
+++ ajax/sample.js
@@ -4,6 +4,20 @@
     url: url,
     dataType: 'jsonp',
     success : function(data){
+      alert('succeeded')
+      target.prop('value',data)
+    },
+    error: function(data){
+      alert('failed')
       target.prop('value',data)
     }
   });
+
+  $.ajax({
+    url: url,
+    dataType: 'jsonp',
+  })
+  .done(function(data){
+      alert('succeeded')
+      target.prop('value',data)
+  })
