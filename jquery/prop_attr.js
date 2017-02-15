108d16479b3e4b630b6a869319c2c4c259fb9c98
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-02-14 22:34:41 -0800

    Update prop_attr.js

--- jquery/prop_attr.js
+++ jquery/prop_attr.js
@@ -11,3 +11,26 @@ $(selector).click(function(){
 
 Use prop() over attr() in the majority of cases.
 
+Read checkbox checked status
+var id="admin_settings" 
+var dom = $('#'+id)
+dom.prop('checked')
+dom.attr('checked')
+dom.attr('checked', true)
+dom.prop('checked', true)
+dom.attr('checked', 'checked')
+
+
+Using jQuery > 1.6
+<input type="checkbox" value="1" name="checkMeOut" id="checkMeOut" checked="checked" />
+// traditional attr
+$('#checkMeOut').attr('checked'); // "checked"
+// new property method
+$('#checkMeOut').prop('checked'); // true
+Using the new property method:
+
+if($('#checkMeOut').prop('checked')) {
+    // something when checked
+} else {
+    // something else when not
+}
