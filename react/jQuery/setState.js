7a88095b2b3d724a7166a58c5b637a7e9446a0de
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-08-15 23:24:34 -0700

    Create setState.js

---
+++ react/jQuery/setState.js
@@ -0,0 +1,10 @@
+https://stackoverflow.com/questions/31856712/update-component-state-from-outside-react-on-server-response
+
+componentWillMount(){
+ window.updateState = (data) => {
+    this.setState({...});     
+  };
+}
+
+in jQuery
+updateState(data);
