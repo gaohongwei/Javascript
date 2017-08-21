7bdc72237ae6dd888364fc4a338acff3cb3f65e4
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-08-20 20:29:06 -0700

    Create router_v4.js

---
+++ react/router/router_v4.js
@@ -0,0 +1,64 @@
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/user'>Roster</Link></li>
+              <li><Link to='/schedule'>Schedule</Link></li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/user' component={User}/>
+            <Route path='/schedule' component={User}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/user'>Roster</Link></li>
+        <li><Link to='/schedule'>Schedule</Link></li>
+      </ul>
+    </nav>
+  </header>
+)
+
+const AppVersion2 = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/user' component={User}/>
+        <Route path='/schedule' component={User}/>
+      </Switch>
+    </div>
+  </BrowserRouter>
+)
+
+class AppVersion1 extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/user' component={User}/>
+            <Route path='/schedule' component={User}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
