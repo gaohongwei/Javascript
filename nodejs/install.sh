7ff5bb0dd0d33dc42881b4531cee303cd55e14d2
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-10-28 15:38:59 -0700

    Update install.sh

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -1,14 +1,29 @@
-Installing Node.js via package manager
-https://nodejs.org/en/download/package-manager/
+##### mac #####
+  npm -v
+  node -v
+  brew install node
+  #brew uninstall node
 
-for Node.js v6:
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+  mkdir node-easy-notes-app
+  cd node-easy-notes-app
+  npm init
 
-for Node.js v7:
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-sudo apt-get install -y nodejs
-Optional: install build tools
+  # Sequelize/Mongoose
+  # Sequelize uses promises and Mongoose does not
+  npm install express body-parser mongoose --save
 
-To compile and install native addons from npm you may also need to install build tools:
-sudo apt-get install -y build-essential
+##### ubuntu #####
+  Installing Node.js via package manager
+  https://nodejs.org/en/download/package-manager/
+
+  for Node.js v6:
+  curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+  sudo apt-get install -y nodejs
+
+  for Node.js v7:
+  curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+  sudo apt-get install -y nodejs
+  Optional: install build tools
+
+  To compile and install native addons from npm you may also need to install build tools:
+  sudo apt-get install -y build-essential
