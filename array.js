ef441a37870ca651e62334c869676d4a6c5fc44b
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-02-10 10:02:48 -0800

    Update array.js

--- array.js
+++ array.js
@@ -2,3 +2,8 @@
 // array.filter(callbackfn), create a new array by selecting elements from original array
 // function callbackfn(value, index, array1)
 // array
+
+// filter and findIndex
+  var selected = {value: 'my_value'}
+  var new_array = array.filter(item => item.value != selected.value );
+  var column_id = array.findIndex(item => item.value == selected.value);
