5ccb6371961c74b410cf9b08688187272f51def9
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-02-09 21:44:26 -0800

    Create radio_group_fun.js

---
+++ react/component/radio_group_fun.js
@@ -0,0 +1,30 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const RadioGroup = props =>
+  <span>
+    {props.value_options.map(
+      (option,index)=>
+        <label key={ index } >
+          <input type='radio'
+            name= {props.input_name}
+            value={option.value}
+            checked={ option.value == props.selected_value}
+            onChange={e=>console.log(e)}
+          />
+          {option.label}
+        </label>
+      )
+    }
+    <br/>selected: {props.selected_value}
+  </span>
+
+
+
+RadioGroup.propTypes = {
+  input_name:     PropTypes.string,
+  value_options:  PropTypes.array,
+  selected_value: PropTypes.string
+};
+
+export default RadioGroup
