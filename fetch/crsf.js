d6276869992eb68acd32c0928b9613e9f1ea8f37
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-02-28 21:32:58 -0800

    Update crsf.js

--- fetch/crsf.js
+++ fetch/crsf.js
@@ -1,6 +1,14 @@
+
 headers= {
         'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
         'Content-Type': 'application/json',
         'Accept': 'application/json',
         'X-Requested-With': 'XMLHttpRequest'
       }
+
+jQuery
+  $.ajaxSetup({
+    headers: {
+      'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+    }
+  });
