4954350362fb306f323236ad389956e07194246a
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-03-27 22:47:15 -0700

    Update expect.jsx

--- react/spec/expect.jsx
+++ react/spec/expect.jsx
@@ -2,6 +2,17 @@
     return shallow(<CallForward {...{...defaultProps, ...overrides}}/>);
   }
 
+    const defaultProps = {
+      mode: 'disabled',
+      getValidationMessages: () => []
+    };
+
+    const onSelected = chai.spy();
+    const component = getComponent(defaultProps, {
+      onChange: onSelected,
+      onClick: chai.spy()
+    });   
+
     const select = component.find('.myclass');
     select.simulate('change', { target: { value: false } });
     expect(onSelected).to.have.been.called;
