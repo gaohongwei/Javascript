11610b7784344507e21d8432466a5650f7836c24
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-06-09 09:17:59 -0700

    Update and rename let.js to variables.js

--- es6/variables.js
+++ es6/variables.js
@@ -1,5 +1,7 @@
-Let define a variable in block level
-var define a variable in fun level
+Let   define a variable in block level
+const define a variable in block level
+var   define a variable in fun level
+
 for (var i = 0; i < 5; i++) {
   setTimeout( ((x) => () => console.log(x))(i), i * 1000 );
 }
