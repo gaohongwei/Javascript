bf4a78ac4260d37865b6d7e6ca2acfe8cb4abc94
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-06-10 17:31:58 -0700

    Update webpack.config.js

--- react/setup/webpack.config.js
+++ react/setup/webpack.config.js
@@ -25,36 +25,3 @@ var config = {
    }
 }
 module.exports = config;
-
-File: index.html
-<!DOCTYPE html>
-<html lang = "en">
-   <head>
-      <meta charset = "UTF-8">
-      <title>React App</title>
-   </head>
-   <body>
-      <div id = "app"></div>
-      <script src = "index.js"></script>
-   </body>
-</html>
-
-File: App.jsx
-import React from 'react';
-class ReactApp extends React.Component {
-   render() {
-      return (
-         <div>
-            Hello World ReactJS!!!
-         </div>
-      );
-   }
-}
-export default App;
-
-File: main.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactApp from './App .jsx';
-
-ReactDOM.render(<App />, document.getElementById('app'));
