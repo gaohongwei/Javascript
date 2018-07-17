e95f00aa6e57e6f33952c03f0ac613b088f08d40
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-07-16 21:18:31 -0700

    Update stateless.jsx

--- react/component/stateless.jsx
+++ react/component/stateless.jsx
@@ -1,5 +1,28 @@
-export default class MyComponent extends React.Component {
+import React from 'react'
+import PropTypes from 'prop-types';
 
+// version 1
+const ProjectTable = (props) => {
+  return ()
+}
+
+ProjectTable.propTypes = {
+  items: PropTypes.array.isRequired
+}
+
+export default ProjectTable;
+
+
+// version 2
+ProjectTable.propTypes = {
+  items: PropTypes.array.isRequired
+}
+export default function ProjectTable(props) {
+
+}
+
+// version 3
+export default class MyComponent extends React.Component {
   part1() {
     return (
       <FormGroup className='selectme'>
