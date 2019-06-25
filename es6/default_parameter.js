e356102fae1007888ab7b3a632fa22756f1919c4
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-06-25 11:52:25 -0700

    Create default_parameter.js

---
+++ es6/default_parameter.js
@@ -0,0 +1,7 @@
+1. Default Parameters in ES6
+  var getAccounts = function(limit = 10) {
+    ...
+  }
+  var link = function(height = 50, color = 'red', url = 'http://capitalone.io') {
+    ...
+  }
