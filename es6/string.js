0b95c99c618aac37fd763ef13e67b0e2cde25260
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-06-25 11:48:11 -0700

    Create string.js

---
+++ es6/string.js
@@ -0,0 +1,15 @@
+Use backticks
+3. String Interpolation, Template Literals in ES6
+
+  var name = `Your name is ${first} ${last}.`
+  var url = `http://localhost:3000/api/messages/${id}`
+
+4. Multi-line Strings in ES6
+  var roadPoem = `Then took the other, as just as fair,
+      And having perhaps the better claim
+      Because it was grassy and wanted wear,
+      Though as for that the passing there
+      Had worn them really about the same,`
+
+  var fourAgreements = `You have the right to be you.
+      You can only be you when you do your best.`
