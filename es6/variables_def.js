e23866e7aebd363d8d90991ef1651566a9da9e1c
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-06-25 14:24:07 -0700

    Update and rename variables.js to variables_def.js

---
+++ es6/variables_def.js
@@ -0,0 +1,14 @@
+const, let, var
+  Let   define a variable in block level
+  const define a variable in block level
+  var   define a variable in fun level
+
+  for (var i = 0; i < 5; i++) {
+    setTimeout( ((x) => () => console.log(x))(i), i * 1000 );
+  }
+
+  for (let i = 0; i < 5; i++) {
+    setTimeout(function() { console.log(i); }, i * 1000 );
+  }
+
+
