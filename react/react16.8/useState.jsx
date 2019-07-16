f5084962988662647333f1f6ce2c4f90e9ae23a3
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-07-15 17:01:25 -0700

    Create useState.jsx

---
+++ react/react16.8/useState.jsx
@@ -0,0 +1,11 @@
+useState
+
+  import React, { Fragment, useEffect, useState } from 'react';
+  const [count, setCount] = useState(0);
+
+
+  It declares a state variable called count, and set it to 0. 
+
+  React will remember its current value between re-renders, 
+
+  If we want to update the current count, we can call setCount.
