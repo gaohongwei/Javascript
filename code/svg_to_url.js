25ed2f667aa37879859e52b42cb660714db819b9
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-08-11 18:21:27 -0700

    Update and rename svg_to_image.js to svg_to_url.js

--- code/svg_to_url.js
+++ code/svg_to_url.js
@@ -1,8 +1,14 @@
 https://dev.to/benjaminblack/using-a-string-of-svg-as-an-image-source-8mo
+Steps:
+  Find the Markup
+  Markup to svg
+  svg to blob
+  blob to url
 
 let svg = `<svg xmlns="http://www.w3.org/2000/svg" ...`;
 let blob = new Blob([svg], {type: 'image/svg+xml'});
 let url = URL.createObjectURL(blob);
+
 let image = document.createElement('img');
 image.src = url;
 image.addEventListener('load', () => URL.revokeObjectURL(url), {once: true});
