46e61ca3265f82a1e0a0fdbcdce339b43043d3c9
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-09-23 17:44:14 -0700

    Create a0_general.js

---
+++ lang/a0_general.js
@@ -0,0 +1,21 @@
+1. string
+  contains a substring?
+  let s1 = 'cl1 cl2'
+  s1.includes('cl1')
+
+  contains a string?
+  let s1 = 'cl1 cl2'
+  let arr = s1.split(' ')
+  arr.includes('cl1')
+
+2. Defined?
+  var v1;
+  var v2 = null;
+  var v3 = 3;
+
+  v1 == undefined
+  v1 == null
+  v2 == undefined
+  v2 == null
+  v3 == null
+
