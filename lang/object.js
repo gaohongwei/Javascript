3a2a2e9c4790b7aeb9d232791d5723f7f90b02f8
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-09-23 17:41:17 -0700

    Create object.js

---
+++ lang/object.js
@@ -0,0 +1,12 @@
+Object?
+  let arr = [];
+  let obj = {};
+  arr instanceof(Object)
+  obj instanceof(Object)
+
+  Array.isArray(arr)
+  Array.isArray(obj)
+
+Access
+  Object.keys(obj)
+  obj.hasOwnProperty('a')
